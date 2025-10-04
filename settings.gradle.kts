cb1fa05a3111b03b1aba1ef545b8a1ae43bfeaa5
Patrick Noone <patricknoone@gmail.com>
2025-10-04 20:15:45 +0100

    Initial commit of GrokBMS project

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,6 +10,7 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
+        maven { url = uri("https://repo.itextpdf.com/release") }
     }
 }
 rootProject.name = "GrokBMS"
