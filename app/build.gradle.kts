404a79fefcd9437cbec6713866b584ee36ea9bbb
Patrick Noone <patricknoone@gmail.com>
2025-10-07 04:28:48 +0100

    Fixed Kotlin version mismatch by updating to Kotlin 2.0.20 and downgrading com.kizitonwose.calendar:view to 2.5.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,6 +10,7 @@ buildscript {
         mavenCentral()
     }
     dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:2.0.20")
         classpath("com.android.tools.build:gradle:8.5.0")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.0.0")
         classpath("com.google.dagger:hilt-android-gradle-plugin:2.51.1")
@@ -51,9 +52,10 @@ android {
 }
 
 dependencies {
-    implementation("androidx.core:core-ktx:1.16.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.10.0")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:2.0.20")
+    implementation("androidx.core:core-ktx:1.13.1")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
     implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
