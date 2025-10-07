4508465177787f77872b2fd4c4b6299d935eb875
Patrick Noone <patricknoone@gmail.com>
2025-10-07 04:39:55 +0100

    Fixed app/build.gradle.kts syntax for dependencies block and ensured Kotlin 2.0.20 compatibility

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,23 +1,9 @@
+
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
     id("org.jetbrains.kotlin.kapt")
 }
-
-buildscript {
-    repositories {
-        google()
-        mavenCentral()
-    }
-    dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-stdlib:2.0.20")
-        classpath("com.android.tools.build:gradle:8.5.0")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.0.0")
-        classpath("com.google.dagger:hilt-android-gradle-plugin:2.51.1")
-    }
-}
-
-apply(plugin = "dagger.hilt.android.plugin")
 
 android {
     namespace = "com.paddyo.bms"
@@ -39,34 +25,32 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
     }
     kotlinOptions {
-        jvmTarget = "17"
-    }
-    buildFeatures {
-        viewBinding = true
-        // Note: Data binding is not enabled. Enable with `dataBinding = true` if needed.
+        jvmTarget = "1.8"
     }
 }
 
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib:2.0.20")
     implementation("androidx.core:core-ktx:1.13.1")
+    implementation("androidx.core:core:1.13.1")
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
-    implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
+    implementation("androidx.recyclerview:recyclerview:1.3.2")
+    implementation("com.kizitonwose.calendar:view:2.5.2")
     implementation("androidx.room:room-runtime:2.6.1")
     implementation("androidx.room:room-ktx:2.6.1")
     kapt("androidx.room:room-compiler:2.6.1")
     implementation("com.google.dagger:hilt-android:2.51.1")
     kapt("com.google.dagger:hilt-compiler:2.51.1")
-    implementation("com.google.code.gson:gson:2.11.0")
-    implementation("com.kizitonwose.calendar:view:2.8.0")
-    implementation("androidx.work:work-runtime-ktx:2.9.1")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
+    implementation("androidx.documentfile:documentfile:1.0.1")
+    implementation("com.google.code.gson:gson:2.10.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
