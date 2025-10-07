b7c338143d07ba0319a987c03b13f40edc726c98
Patrick Noone <patricknoone@gmail.com>
2025-10-07 22:17:09 +0100

    Fixed Room database issues: added TypeConverters, indices for foreign keys, and corrected AppDatabase/CustomerDao

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,7 +51,7 @@ dependencies {
     implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
     implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
     implementation("androidx.documentfile:documentfile:1.0.1")
-    implementation("com.google.code.gson:gson:2.10.1")
+    implementation("com.google.code.gson:gson:2.11.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
