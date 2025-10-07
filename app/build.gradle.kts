1cd0c558ab7da58499a439bbd85f873f68b5d4dd
Patrick Noone <patricknoone@gmail.com>
2025-10-07 03:39:33 +0100

    Fixed com.android.application plugin version conflict, updated Gradle to 8.5.0, Kotlin to 1.9.24, and calendar:view:2.8.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
-    id("com.android.application") version "8.5.0"
-    id("org.jetbrains.kotlin.android") version "2.0.0"
+    id("com.android.application")id("org.jetbrains.kotlin.android") version "2.0.0"
     id("org.jetbrains.kotlin.kapt") version "2.0.0"
 }
 
