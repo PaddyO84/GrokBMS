a13230d68b31176dbe2c4158de7166715d77839a
Patrick Noone <patricknoone@gmail.com>
2025-10-08 14:22:36 +0100

    Upgraded to Gradle 8.9 and fixed KSP plugin conflict

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,9 +1,9 @@
 
 plugins {
-    id("com.android.application")
-    id("org.jetbrains.kotlin.android")
+    id("com.android.application") version "8.7.0" apply false
+    id("org.jetbrains.kotlin.android") version "2.0.20" apply false
     id("com.google.devtools.ksp") version "2.0.20-1.0.24"
-    id("com.google.dagger.hilt.android")
+    id("com.google.dagger.hilt.android") version "2.52" apply false
 }
 
 android {
